4d323a3b3f4660b91f6b9e9e984e4c6164c3a793
AndrewBeers <andrew_beers@alumni.brown.edu>
2017-09-27 15:49:08 -0400

    docs: Remove old tests

--- test_script.py
+++ test_script.py
@@ -1,33 +1,9 @@
 from qtim_gbmSegmenter.Config_Library.docker_wrapper import docker_segmentation
 
-T2 = '/home/administrator/test_data/TMZ/T2'
-T1 = '/home/administrator/test_data/TMZ/T1'
-T1POST = '/home/administrator/test_data/TMZ/T1POST'
-FLAIR = '/home/administrator/test_data/TMZ/FLAIR'
-OUTPUT = '/home/administrator/test_data/TATA'
-
-# T2 = '/home/administrator/test_data/TATA/T2SPACE_nobias_isotropic_reg.nii.gz'
-# T1 = '/home/administrator/test_data/TATA/T1AxialPremosaicwithSAT_nobias_isotropic_reg.nii.gz'
-# T1POST = '/home/administrator/test_data/TATA/T1AxialPostmosaic_nobias_isotropic_reg.nii.gz'
-# FLAIR = '/home/administrator/test_data/TATA/FLAIRmosaic_nobias_isotropic_reg.nii.gz'
-# OUTPUT = '/home/administrator/test_data/TATA'
-
-# T2 = '/home/administrator/test_data/TATA/TATA_T2.nii.gz'
-# T1 = '/home/administrator/test_data/TATA/TATA_T1.nii.gz'
-# T1POST = '/home/administrator/test_data/TATA/TATA_T1C.nii.gz'
-# FLAIR = '/home/administrator/test_data/TATA/TATA_FLAIR.nii.gz'
-# OUTPUT = '/home/administrator/test_data/TATA'
-
-# T2 = '/home/administrator/test_data/TATA/TATA_T2_nobias_isotropic_reg.nii.gz'
-# T1 = '/home/administrator/test_data/TATA/TATA_T1_nobias_isotropic_reg.nii.gz'
-# T1POST = '/home/administrator/test_data/TATA/TATA_T1C_nobias_isotropic_reg.nii.gz'
-# FLAIR = '/home/administrator/test_data/TATA/TATA_FLAIR_nobias_isotropic_reg.nii.gz'
-# OUTPUT = '/home/administrator/test_data/TATA'
-
-# T2 = '/home/administrator/test_data/NORMALIZED_NIFTI/TATA_T2_nobias_isotropic_reg_ss_normalized.nii.gz'
-# T1 = '/home/administrator/test_data/NORMALIZED_NIFTI/TATA_T1_nobias_isotropic_reg_ss_normalized.nii.gz'
-# T1POST = '/home/administrator/test_data/NORMALIZED_NIFTI/TATA_T1C_nobias_isotropic_reg_ss_normalized.nii.gz'
-# FLAIR = '/home/administrator/test_data/NORMALIZED_NIFTI/TATA_FLAIR_nobias_isotropic_reg_ss_normalized.nii.gz'
-# OUTPUT = '/home/administrator/test_data/TATA'
+T2 = '/home/user/input_folder/T2'
+T1 = '/home/user/input_folder/T1'
+T1POST = '/home/user/input_folder/T1POST'
+FLAIR = '/home/user/input_folder/FLAIR'
+OUTPUT = '/home/user/output_folder'
 
 docker_segmentation(T2, T1, T1POST, FLAIR, OUTPUT)
